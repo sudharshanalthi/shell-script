e153869138c6f0ba1f9833f08ea023047f900e29
althi@sudharshan <althi@sudharshan.com>
2023-12-30 19:17:51 +0530

    added 2nd script

---
+++ 06-logs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+echo "script name: $0"
+
+VALIDATE(){
+if [ $? -ne 0 ]
+    then
+        echo "ERROR:: Installing  is failed"
+        exit 1
+    else 
+        echo "Installing is success"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo "ERROR:: please run this script with root access"
+else
+    echo "you are root user"
+    fi 
+
+    yum install mysql -y
+
+    VALIDATE
+    
+    yum install git -y
