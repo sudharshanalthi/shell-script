2aab1b25e80fcba6e3a26ca2470361e14038ed63
althi@sudharshan <althi@sudharshan.com>
2023-12-30 15:53:37 +0530

    added 2nd script

---
+++ 05-functions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+VALIDATE(){
+if [ $? -ne 0 ]
+    then
+        echo "ERROR:: Installing Mysql is failed"
+        exit 1
+    else 
+        echo "Installing Mysql is success"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo "ERROR:: please run this script with root access"
+else
+    echo "you are root user"
+    fi 
+
+    Yum install Mysql -y
+
+    VALIDATE
+
+    Yum install git -y
