ad6ace6cc6e71f2c0625b9b43d44171940c31128
althi@sudharshan <althi@sudharshan.com>
2023-12-30 16:03:00 +0530

    added 2nd script

--- 05-functions.sh
+++ 05-functions.sh
@@ -5,10 +5,10 @@ ID=$(id -u)
 VALIDATE(){
 if [ $? -ne 0 ]
     then
-        echo "ERROR:: Installing Mysql is failed"
+        echo "ERROR:: Installing  is failed"
         exit 1
     else 
-        echo "Installing Mysql is success"
+        echo "Installing is success"
     fi
 }
 
@@ -19,10 +19,6 @@ else
     echo "you are root user"
     fi 
 
-    Yum install mysql -y
-
-    VALIDATE
-
-    Yum install git -y
+    yum install mysql -y
 
     VALIDATE
