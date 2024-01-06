d079a4e0dec82d961e45383c687077f96f46c017
althi@sudharshan <althi@sudharshan.com>
2024-01-06 14:22:51 +0530

    added 2nd script

--- 08-install-packages.sh
+++ 08-install-packages.sh
@@ -5,14 +5,43 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+   if [ $1 -ne 0 ]
+   then
+       echo -e "$2 ... $R FAILED $N"
+   else
+       echo -e "$2 ... $G SUCCESS $N"
+       fi
+}
+
+
 if [ $ID -ne 0 ]
 then
-   echo -e "$R error:: please run this script without root access $N"
+   echo -e "$R ERROR:: please run this script without root access $N"
    exit 1 # you can give other than 0
-   else
+else
        echo "you are root user"
     fi # fi means reverse of if, indicating condition end
 
-    echo " ALL arguments passed: $@"
+   # echo " ALL arguments passed: $@"
+   # git mysql postfix net-tools
+   # package=git for first time
+
+   for package in $@
+   do
+       yum list installed $package &>> $LOGFILE # check installed or not
+       if [ $? -ne 0 ] #if not installed 
+       then
+           yum install $package -y &>> $LOGFILE # install the package
+       VALIDATE $? "Installation OF $package" # validate
+   else
+       echo -e "$package is already installed ... $Y SKIPPING $N"
+       fi
+   done
     
 
