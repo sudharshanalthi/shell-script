5c91a3d27dbfae8ee844c5e5ce4ed9348e061cb7
althi@sudharshan <althi@sudharshan.com>
2024-01-06 13:40:44 +0530

    added 2nd script

--- 08-install-packages.sh
+++ 08-install-packages.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-ID=$(id -U)
+ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
 if [ $ID -ne 0 ]
 then
-   echco -e "$R error:: please run this script without root access $N"
-   exit 1 # uou can give other than 0
+   echo -e "$R error:: please run this script without root access $N"
+   exit 1 # you can give other than 0
    else
        echo "you are root user"
     fi # fi means reverse of if, indicating condition end
