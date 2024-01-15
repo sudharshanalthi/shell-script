598588fde52cfccecfe3e8df2ba2533a1d2d92be
althi@sudharshan <althi@sudharshan.com>
2024-01-15 23:53:45 +0530

    daws-76

--- 11-disk-usage.sh
+++ 11-disk-usage.sh
@@ -6,8 +6,8 @@ message=""
 
 while IFS= read line
 do
-     usage=$(echo $line | awk '{print $6f}' | cut -d % -f1)
-     partition=$(echo $line | awk '{print $1f}' )
+     usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
+     partition=$(echo $line | awk '{print $1F}' )
      if [ $usage -ge $DISK_THRESHOLD ]
      then
         message+="High Disk Usage on $partition: $usage"
